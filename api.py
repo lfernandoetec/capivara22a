e8f24395a4ddbb3d77e6fd1a3c901d8778b26127
unknown <you@example.com>
2022-11-17 22:30:46 -0300

    Update api.py

--- api.py
+++ api.py
@@ -6,13 +6,24 @@ import pprint
 bunitinho =  pprint.pprint
 # dominio e path base da api
 domain = 'https://dadosabertos.camara.leg.br/api/v2/'
-# recurso a ser utilizado
-path = 'deputados'
-# endereco a ser pesquisado
-url = domain + path
-# faz um get no site dados abertos da camara e obtem a listagem dos deputados
-r = requests.get(url)
-# imprime o status code
-print(r.status_code)
-# imprime o conteudo json formatado pra ficar bonito com a pprint
-bunitinho(r.json())
+
+def busca_deputado(): 
+    # recurso a ser utilizado
+    path = 'deputados'
+    # endereco a ser pesquisado
+    url = domain + path
+    # parametros
+    params = {'nome': '', 'ordem': 'asc', 'ordenarPor': 'id'}
+    params['nome'] = input('qual o nome do deputado:')
+    # faz um get no site dados abertos da camara e obtem a listagem dos deputados
+    r = requests.get(url, params)
+    # imprime o status code
+    #print(r.status_code)
+    # imprime o conteudo json formatado pra ficar bonito com a pprint
+    #bunitinho(r.json())
+    data = r.json()
+    # imprime somente o nome e o id do deputado
+    for deputado in data['dados']:
+        print('nome:',deputado['nome'],' id:',deputado['id'])
+
+busca_deputado()
